c6ddd4f03665c98dae4da8ec96e9c70fca858c78
eevajonnapanula <eeva-jonna.panula@codento.com>
2020-02-29 10:15:16 +0200

    Add Now page and menu and favicon

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const navButton = document.querySelector('nav button')
 navButton.addEventListener('click', function () {
   const expanded = this.getAttribute('aria-expanded') === 'true'
   this.setAttribute('aria-expanded', !expanded)
-  const menu = this.nextElementSibling
-  menu.hidden = !menu.hidden
 })
