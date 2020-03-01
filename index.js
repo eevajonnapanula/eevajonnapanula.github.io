68e441636c9182aefa02e036dadacd925c1a9029
eevajonnapanula <eeva-jonna.panula@codento.com>
2020-03-01 10:02:42 +0200

    Add dark modes first version

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ navButton.addEventListener('click', function () {
   const expanded = this.getAttribute('aria-expanded') === 'true'
   this.setAttribute('aria-expanded', !expanded)
 })
+
+const themeCheck = document.getElementById('theme')
+
+themeCheck.addEventListener('change', function (event) {
+  event.target.checked ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+})
