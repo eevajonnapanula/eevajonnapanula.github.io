3729092142901197f0fb0185a81efebc6202a884
eevajonnapanula <eeva-jonna.panula@codento.com>
2020-03-02 15:28:04 +0200

    Add dark theme switcher

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const navButton = document.querySelector('nav button')
-
-navButton.addEventListener('click', function () {
-  const expanded = this.getAttribute('aria-expanded') === 'true'
-  this.setAttribute('aria-expanded', !expanded)
-})
-
 const themeCheck = document.getElementById('theme')
+const themeCheckWrapper = document.getElementsByClassName('theme-switch-we')
+function addOrRemoveDarkTheme(condition) {
+  if (condition) {
+    document.querySelector('body').classList.add('dark')
+  } else {
+    document.querySelector('body').classList.remove('dark')
+  }
+}
 
-themeCheck.addEventListener('change', function (event) {
-  event.target.checked ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+document.addEventListener('DOMContentLoaded', function () {
+  themeCheck.checked = localStorage.getItem('darkMode') === 'true'
+  addOrRemoveDarkTheme(localStorage.getItem('darkMode') === 'true')
+
+  themeCheck.addEventListener('change', function (event) {
+    localStorage.setItem('darkMode', event.currentTarget.checked)
+    themeCheck.setAttribute('checked', !themeCheck.checked)
+    addOrRemoveDarkTheme(event.currentTarget.checked)
+  })
 })
